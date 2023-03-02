3ba59de60ef14def8ec17c3994176487c58ef880
BelovRV89 <belov.ruslan@list.ru>
2023-03-02 10:15:32 +0300

    First_2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (
-    IsAuthenticated,
     IsAuthenticatedOrReadOnly
 )
 from rest_framework import filters
